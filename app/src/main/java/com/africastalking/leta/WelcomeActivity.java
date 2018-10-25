196306b3d2a7b7b90a49c2d2524d28061be28017
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-10-25 11:40:04 +0300

    add readme

--- app/src/main/java/com/africastalking/leta/WelcomeActivity.java
+++ app/src/main/java/com/africastalking/leta/WelcomeActivity.java
@@ -17,7 +17,7 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        btnsub = (Button)findViewById(R.id.buttonsub);
+        btnsub = (Button)findViewById(R.id.buttonGetStarted);
         l1 = (LinearLayout) findViewById(R.id.l1);
         l2 = (LinearLayout) findViewById(R.id.l2);
         uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
