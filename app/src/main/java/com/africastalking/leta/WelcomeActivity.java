934fcd3547957e1f7d040b0a3f177e5d95a57047
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-10-25 11:55:27 +0300

    clean up welcome java code

--- app/src/main/java/com/africastalking/leta/WelcomeActivity.java
+++ app/src/main/java/com/africastalking/leta/WelcomeActivity.java
@@ -8,18 +8,18 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    //declare uptodown and downtoup layouts, get started button
     LinearLayout l1,l2;
-    Button btnsub;
+    Button getStartedBtn;
     Animation uptodown,downtoup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        btnsub = (Button)findViewById(R.id.buttonGetStarted);
-        l1 = (LinearLayout) findViewById(R.id.l1);
-        l2 = (LinearLayout) findViewById(R.id.l2);
+        getStartedBtn = findViewById(R.id.buttonGetStarted);
+        l1 = findViewById(R.id.l1);
+        l2 = findViewById(R.id.l2);
         uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
         downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
         l1.setAnimation(uptodown);
