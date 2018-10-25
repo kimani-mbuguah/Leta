b954d0cd03fd830d72e1e10e0b6452a98e9ed74c
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-10-25 11:58:55 +0300

    add client and vendor login activities

---
+++ app/src/main/java/com/africastalking/leta/ClientLoginActivity.java
@@ -0,0 +1,13 @@
+package com.africastalking.leta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClientLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_login);
+    }
+}
