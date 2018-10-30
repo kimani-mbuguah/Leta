007832d814a35113074ae244ab3144bc79f2f764
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-10-30 16:10:52 +0300

    add MyCart activity

---
+++ app/src/main/java/com/africastalking/leta/MyCartActivity.java
@@ -0,0 +1,13 @@
+package com.africastalking.leta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_cart);
+    }
+}
