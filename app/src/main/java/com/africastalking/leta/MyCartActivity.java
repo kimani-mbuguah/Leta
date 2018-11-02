d39653718e353edb325a2b73481ded23fe8beab2
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-11-02 10:44:08 +0300

    design payment options layout and link checkout button to payment options activity

--- app/src/main/java/com/africastalking/leta/MyCartActivity.java
+++ app/src/main/java/com/africastalking/leta/MyCartActivity.java
@@ -1,20 +1,33 @@
 package com.africastalking.leta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MyCartActivity extends AppCompatActivity {
     private RecyclerView cartItemsRecyclerView;
     ArrayList<CartItem> cartItems;
+    private Button btnContinueToCheckout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_cart);
+
+        btnContinueToCheckout = findViewById(R.id.btnContinueToCheckout);
+        btnContinueToCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent choosePaymentMethod = new Intent(MyCartActivity.this, PaymentOptionsActivity.class);
+                startActivity(choosePaymentMethod);
+            }
+        });
 
         cartItems = new ArrayList<>();
         cartItems.add(new CartItem("Ugali Matumbo", "Kibanda1", 300,3));
