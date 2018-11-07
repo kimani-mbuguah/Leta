1ffb14bd63dc21720086501b8e15ff20ddf8a2dc
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-11-07 15:23:55 +0300

    create click listener to listen to test add new item activity

--- app/src/main/java/com/africastalking/leta/MyMealsFragment.java
+++ app/src/main/java/com/africastalking/leta/MyMealsFragment.java
@@ -1,17 +1,20 @@
 package com.africastalking.leta;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MyMealsFragment extends Fragment {
+    private Button testBtn;
 
 
     public MyMealsFragment() {
@@ -22,8 +25,17 @@ public class MyMealsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_my_meals,container, false);
+        testBtn = view.findViewById(R.id.test_button);
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent testAddItemActivity = new Intent(getActivity().getApplicationContext(),NewItemActivity.class);
+                startActivity(testAddItemActivity);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_meals, container, false);
+        return view;
     }
 
 }
