adbe4ca4ac7b0d61bb523140046e017c6db9129b
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-11-19 15:09:29 +0300

    implement methods in my meals adapter

---
+++ app/src/main/java/com/africastalking/leta/MyMeals.java
@@ -0,0 +1,21 @@
+package com.africastalking.leta;
+
+public class MyMeals {
+    public String item_id;
+
+    public MyMeals(String item_id) {
+        this.item_id = item_id;
+    }
+
+    public MyMeals() {
+    }
+
+    public String getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(String item_id) {
+        this.item_id = item_id;
+    }
+
+}
--- app/src/main/java/com/africastalking/leta/MyMealsAdapter.java
+++ app/src/main/java/com/africastalking/leta/MyMealsAdapter.java
@@ -1,30 +1,65 @@
 package com.africastalking.leta;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
 
 public class MyMealsAdapter extends RecyclerView.Adapter<MyMealsAdapter.ViewHolder> {
+    Dialog mDialog;
+    public List<MyMeals> MyMealsList;
+    public Context context;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+
+    public MyMealsAdapter(List<MyMeals> MyMealsList){
+        this.MyMealsList = MyMealsList;
+    }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.my_meals_layout,viewGroup,false);
+        context = viewGroup.getContext();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView itemName,itemDesc,itemPrice,removeItem,restaurantName;
+        public Button moreButton;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            itemName = itemView.findViewById(R.id.my_meals_item_name);
+            itemDesc = itemView.findViewById(R.id.my_meals_item_desc);
+            itemPrice = itemView.findViewById(R.id.my_meals_item_price);
+            removeItem = itemView.findViewById(R.id.my_meals_txtclose);
+            restaurantName = itemView.findViewById(R.id.my_meals_restaurant_name);
+            moreButton = itemView.findViewById(R.id.my_meals_moreBtn);
         }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        MyMeals myMeals = MyMealsList.get(position);
+        //final String
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return MyMealsList.size();
     }
 }
