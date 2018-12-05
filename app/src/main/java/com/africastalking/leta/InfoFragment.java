69ec6c4ce5410891a41c0ac71ca5fa0cf2f6fceb
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-12-05 17:41:17 +0300

    design restaurant info ui

--- app/src/main/java/com/africastalking/leta/InfoFragment.java
+++ app/src/main/java/com/africastalking/leta/InfoFragment.java
@@ -6,13 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InfoFragment extends Fragment {
-
+    private RatingBar mRatingBar;
 
     public InfoFragment() {
         // Required empty public constructor
@@ -23,7 +24,10 @@ public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View view =  inflater.inflate(R.layout.fragment_info, container, false);
+        mRatingBar = view.findViewById(R.id.restaurantRatingBar);
+        mRatingBar.setRating(Float.parseFloat("2.0"));
+        return view;
     }
 
 }
